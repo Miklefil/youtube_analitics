bcc9986c6ceb99a6b8d1ca856832ca6d1f373fe5
twist <twistysmig@mail.ru>
2023-03-20 14:33:17 +0300

    Task six done

--- class_video.py
+++ class_video.py
@@ -5,11 +5,19 @@ from googleapiclient.discovery import build
 class Video:
     def __init__(self, video_id):
         self.video_id = video_id
-        youtube = self.get_service()
-        self.video_response = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
-        self.title = self.video_response['items'][0]['snippet']['title']
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            youtube = self.get_service()
+            self.video_response = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+        except Exception:
+            self.video_response = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+
 
     @classmethod
     def get_service(cls):
@@ -18,5 +26,17 @@ class Video:
         return youtube
 
     def __str__(self):
-        return self.title
+        try:
+            if self.title is not None:
+                return self.title
+            else:
+                raise TypeError
+        except TypeError:
+            return f'Невозможно получить данные по этому IDшнику'
+
+video1 = Video('broken_video_id')
+
+print(video1)
+print(video1.title)
+print(video1.like_count)
 
